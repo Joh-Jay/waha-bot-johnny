68f8187d4127049a7dde6ee307580d19621481dd
Joh-Jay <johnny.silva@turpass.com.br>
2025-08-02 01:12:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-
 const { create } = require('@open-wa/wa-automate');
 
 create({
     sessionId: "johnny-session",
-    multiDevice: true, // true for MD
-    qrTimeout: 0, // show QR until scanned
+    multiDevice: true,
+    qrTimeout: 0,
     authTimeout: 60,
     headless: true,
     logConsole: true,
     devtools: false,
-    popup: true,
-    useChrome: true,
-    disableSpins: false,
+    popup: false,
+    useChrome: false, // ALTERADO de true para false
+    disableSpins: true,
     disableWelcome: true,
-    cacheEnabled: false
+    cacheEnabled: false,
+    executablePath: '/usr/bin/chromium-browser', // ADICIONADO
+    args: ['--no-sandbox', '--disable-setuid-sandbox'] // ADICIONADO
 }).then(client => start(client));
 
 function start(client) {
     console.log("Bot iniciado com sucesso.");
     client.onMessage(async message => {
-        if (message.body === 'Oi') {
+        if (message.body.toLowerCase() === 'oi') {
             await client.sendText(message.from, 'Olá! Como posso te ajudar hoje?');
         }
     });
